d224e1d8d0bcb29d1cc7b9e0f05b0104418c8303
Super262 <virusformail@gmail.com>
2021-03-07 20:51:49 +0800

    Finished Problem 200.

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def test():
+    print(max((4,), (3, 1)))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    test()
--- problem0200.py
+++ problem0200.py
@@ -4,9 +4,10 @@ class Solution:
     @return: a string as the longest palindromic substring
     """
 
-    def longestPalindrome(self, s):
+    def longestPalindrome1(self, s):
+        # Dynamic Programming
         if not s:
-            return ""
+            return s
         n = len(s)
         is_palindrome = [[False] * n for _ in range(n)]
         for i in range(n):
@@ -22,3 +23,19 @@ class Solution:
                     start = i
                     longest = length
         return s[start:start + longest]
+
+    def longestPalindrome2(self, s):
+        # Iteration from the center.
+        if not s:
+            return s
+        answer = (1, 0)
+        for mid in range(len(s)):
+            answer = max(answer, self.get_longest_palindrome_from(s, mid, mid))
+            answer = max(answer, self.get_longest_palindrome_from(s, mid, mid + 1))
+        return s[answer[1]:answer[1] + answer[0]]
+
+    def get_longest_palindrome_from(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1, left + 1

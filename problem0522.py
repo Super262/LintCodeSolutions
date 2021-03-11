0934ab5d563e6fc122847022331ebf4947ba4c61
Super262 <virusformail@gmail.com>
2021-03-11 14:44:00 +0800

    Finished Problem 522.

--- problem0522.py
+++ problem0522.py
@@ -1,57 +1,43 @@
 class TinyUrl2:
     def __init__(self, tiny_url_len=6):
-        self.next_id = 0
-        self.data = {}
-        self.key = {}
         self.chars = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
                       'z', 'x', 'c', 'v', 'b', 'n', 'm', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Q', 'W',
                       'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X',
                       'C', 'V', 'B', 'N', 'M']
+        self.next_id = 0
+        self.short_to_long = {}
+        self.long_to_short = {}
         self.base = len(self.chars)
         self.tiny_url_len = tiny_url_len
         self.common_prefix = "http://tiny.url/"
 
     def createCustom(self, long_url, key: str) -> str:
-        current_id = self.sixty_two_to_ten(key)
-        if long_url in self.data.values():
-            if current_id not in self.data or self.data[current_id] != long_url:
-                return "error"
-        else:
-            if current_id in self.data:
-                return "error"
-        self.data[current_id] = long_url
-        self.key[current_id] = key
-        return self.common_prefix + key
+        if key in self.short_to_long and self.short_to_long[key] == long_url:
+            return self.common_prefix + key
+        if key not in self.short_to_long and long_url not in self.long_to_short:
+            self.short_to_long[key] = long_url
+            self.long_to_short[long_url] = key
+            return self.common_prefix + key
+        return "error"
 
     def longToShort(self, url: str) -> str:
-        for id, value in self.data.items():
-            if value == url:
-                return self.common_prefix + self.key[id]
+        if url in self.long_to_short:
+            return self.common_prefix + self.long_to_short[url]
         current_id = self.next_id
-        while current_id in self.data:
+        current_key = self.ten_to_sixty_two(current_id)
+        while current_key in self.short_to_long:
             current_id += 1
-        self.data[current_id] = url
+            current_key = self.ten_to_sixty_two(current_id)
         self.next_id = current_id + 1
-        self.key[current_id] = self.ten_to_sixty_two(current_id)
-        return self.common_prefix + self.key[current_id]
+        self.long_to_short[url] = current_key
+        self.short_to_long[current_key] = url
+        return self.common_prefix + current_key
 
     def shortToLong(self, url: str) -> str:
-        current_id = self.sixty_two_to_ten(url[len(self.common_prefix):])
-        if current_id in self.data:
-            return self.data[current_id]
-        else:
-            return "error"
-
-    def sixty_two_to_ten(self, val: str) -> int:
-        current_id = 0
-        current_factor = 1
-        chars_list = list(val)
-        chars_list.reverse()
-        for c in chars_list:
-            digit = self.index_in_chars(c)
-            current_id += digit * current_factor
-            current_factor *= self.base
-        return current_id
+        url = url[len(self.common_prefix):]
+        if url in self.short_to_long:
+            return self.short_to_long[url]
+        return "error"
 
     def ten_to_sixty_two(self, val: int) -> str:
         result = []
@@ -62,9 +48,3 @@ class TinyUrl2:
             result.append(self.chars[0])
         result.reverse()
         return "".join(result)
-
-    def index_in_chars(self, c: str) -> int:
-        for i in range(self.base):
-            if self.chars[i] == c:
-                return i
-        return -1

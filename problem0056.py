004b2448f3b65e2a9e9fa29a3643afd5a6d5b361
Super262 <virusformail@gmail.com>
2021-03-15 10:15:08 +0800

    Finished Problem 56.

---
+++ problem0056.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    @param numbers: An array of Integer
+    @param target: target = numbers[index1] + numbers[index2]
+    @return: [index1 + 1, index2 + 1] (index1 < index2)
+    """
+
+    def twoSum(self, numbers: list, target: int) -> list:
+        numbers_dict = dict()
+        expected_set = set()
+        for i in range(len(numbers)):
+            if numbers[i] in numbers_dict:
+                # For repeating elements, only remember the first one.
+                continue
+            numbers_dict[numbers[i]] = i
+        for i in range(len(numbers)):
+            t = target - numbers[i]
+            if t in expected_set:
+                return [numbers_dict[t], i]
+            expected_set.add(numbers[i])
+        return []

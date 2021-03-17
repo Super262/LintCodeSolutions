192c950dafd1494d7fa615af9717c6f165f06829
Super262 <virusformail@gmail.com>
2021-03-17 13:31:16 +0800

    Finished Problem 585.

---
+++ problem0585.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    @param nums: a mountain sequence which increase firstly and then decrease
+    @return: then mountain top
+    """
+
+    def mountainSequence(self, nums: list) -> int:
+        if not nums:
+            return -1
+        start = 0
+        end = len(nums) - 1
+        while start < end:
+            mid = start + (end - start) // 2
+            if mid + 1 <= end and nums[mid + 1] >= nums[mid]:
+                start = mid + 1
+                continue
+            if mid - 1 >= start and nums[mid - 1] >= nums[mid]:
+                end = mid - 1
+                continue
+            return nums[mid]
+        return nums[start]

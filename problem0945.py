e7906c4fc50931f43ca889349616e9699d887240
Super262 <virusformail@gmail.com>
2021-03-20 10:58:00 +0800

    Finished Problem 945.

---
+++ problem0945.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def leastInterval(self, tasks: list, n: int) -> int:
+        if n == 0:
+            return len(tasks)
+        task_frequency = [0] * 26
+        for t in tasks:
+            task_frequency[ord(t) - ord("A")] += 1
+        task_frequency.sort(reverse=True)
+        last_part_len = 0
+        for i in range(len(task_frequency)):
+            if task_frequency[i] == task_frequency[0]:
+                last_part_len += 1
+        return max(len(tasks), (task_frequency[0] - 1) * (n + 1) + last_part_len)

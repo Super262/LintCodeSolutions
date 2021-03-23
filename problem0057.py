3c6a4144c98a2f0051c147617c09a872cdeee3ed
Super262 <virusformail@gmail.com>
2021-03-23 09:06:26 +0800

    Finished Problem 57.

--- problem0057.py
+++ problem0057.py
@@ -6,8 +6,11 @@ class Solution:
         results = []
         sorted_arr = sorted(numbers)
         for i in range(len(sorted_arr)):
+
+            # Don't forget this part!
             if i > 0 and sorted_arr[i] == sorted_arr[i - 1]:
                 continue
+
             self.two_sum(sorted_arr, i + 1, len(sorted_arr) - 1, -sorted_arr[i], results)
         return results
 
@@ -17,8 +20,11 @@ class Solution:
                 results.append([-target, sorted_arr[left], sorted_arr[right]])
                 right -= 1
                 left += 1
+
+                # Don't forget this part!
                 while left < right < len(sorted_arr) - 1 and sorted_arr[right] == sorted_arr[right + 1]:
                     right -= 1
+
             elif sorted_arr[left] + sorted_arr[right] > target:
                 right -= 1
             else:

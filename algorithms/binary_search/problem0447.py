af712149c607922b26c65fa4dcf1412d52aa0f05
Super262 <virusformail@gmail.com>
2021-03-24 12:16:16 +0800

    Finished Problem 447.

---
+++ algorithms/binary_search/problem0447.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def searchBigSortedArray(self, reader, target: int) -> int:
+        kth = 1
+        while reader.get(kth - 1) < target:
+            kth *= 2
+        start = 0
+        end = kth - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if reader.get(mid) < target:
+                start = mid
+            else:
+                end = mid
+        if reader.get(start) == target:
+            return start
+        if reader.get(end) == target:
+            return end
+        return -1

cea2ec8d237ea7978fd5e71de37fd80fb29b4b8a
Super262 <virusformail@gmail.com>
2021-03-26 12:19:03 +0800

    Finished Problem 901.

--- data_structures/bst/problem0901.py
+++ data_structures/bst/problem0901.py
@@ -12,5 +12,57 @@ class Solution:
     @return: k values in the BST that are closest to the target
     """
 
-    def closestKValues(self, root, target, k):
+    def closestKValues(self, root: TreeNode, target: float, k: int) -> list:
+        result = []
+        if not root:
+            return result
+        lower_stack = self.initialize_stack(root, target)
+        upper_stack = list(lower_stack)
+        if target < float(lower_stack[-1].val):
+            self.move_lower(lower_stack)
+        else:
+            self.move_upper(upper_stack)
+        for _ in range(k):
+            if len(lower_stack) == 0 or (len(upper_stack) > 0 and
+                                         abs(float(lower_stack[-1].val) - target) > abs(
+                        float(upper_stack[-1].val - target))):
+                result.append(upper_stack[-1].val)
+                self.move_upper(upper_stack)
+            else:
+                result.append(lower_stack[-1].val)
+                self.move_lower(lower_stack)
+        return result
 
+    def initialize_stack(self, root: TreeNode, target: float) -> list:
+        stack = []
+        while root:
+            stack.append(root)
+            if float(root.val) > target:
+                root = root.left
+            else:
+                root = root.right
+        return stack
+
+    def move_upper(self, stack: list) -> None:
+        node = stack[-1]
+        if not node.right:
+            node = stack.pop()
+            while len(stack) > 0 and stack[-1].right == node:
+                node = stack.pop()
+            return
+        node = node.right
+        while node:
+            stack.append(node)
+            node = node.left
+
+    def move_lower(self, stack: list) -> None:
+        node = stack[-1]
+        if not node.left:
+            node = stack.pop()
+            while len(stack) > 0 and stack[-1].left == node:
+                node = stack.pop()
+            return
+        node = node.left
+        while node:
+            stack.append(node)
+            node = node.right

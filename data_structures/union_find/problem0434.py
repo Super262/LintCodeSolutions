3bdde10d2b215a33b623c583dca02219f3f52828
Super262 <virusformail@gmail.com>
2021-03-27 10:00:53 +0800

    Finished Problem 434.

--- data_structures/union_find/problem0434.py
+++ data_structures/union_find/problem0434.py
@@ -8,40 +8,42 @@ class Solution:
 
     def numIslands2(self, n: int, m: int, operators: list) -> list:
         directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-        matrix = []
-        father = {}
-        for _ in range(n):
-            matrix.append([0] * m)
+        matrix = [0] * (n * m)
+        father = [i for i in range(n * m)]
         result = []
         for p in operators:
-            if matrix[p.x][p.y] == 1:
+            if matrix[self.point_to_int(p.x, p.y, m)] == 1:
                 result.append(result[-1])
                 continue
-            matrix[p.x][p.y] = 1
-            father[(p.x, p.y)] = (p.x, p.y)
+            matrix[self.point_to_int(p.x, p.y, m)] = 1
+            father[self.point_to_int(p.x, p.y, m)] = self.point_to_int(p.x, p.y, m)
             temp = 1
             if len(result) > 0:
                 temp += result[-1]
             for d in directions:
-                if n > p.x + d[0] >= 0 and 0 <= p.y + d[1] < m and matrix[p.x + d[0]][p.y + d[1]] == 1:
-                    root_a = self.find(father, (p.x, p.y))
-                    root_b = self.find(father, (p.x + d[0], p.y + d[1]))
+                next_x = p.x + d[0]
+                next_y = p.y + d[1]
+                if n > next_x >= 0 and 0 <= next_y < m and matrix[self.point_to_int(next_x, next_y, m)] == 1:
+                    root_a = self.find(father, self.point_to_int(p.x, p.y, m))
+                    root_b = self.find(father, self.point_to_int(next_x, next_y, m))
                     if root_b != root_a:
                         self.connect(father, root_a, root_b)
                         temp -= 1
             result.append(temp)
         return result
 
-    def connect(self, father: dict, a: tuple, b: tuple) -> None:
-        father[b] = a
-        self.find(father, a)
-        self.find(father, b)
+    def connect(self, father: list, a_index: int, b_index: int) -> None:
+        father[b_index] = a_index
+        self.find(father, b_index)
 
-    def find(self, father: dict, node: tuple) -> tuple:
+    def find(self, father: list, nodeIndex: int) -> int:
         path = []
-        while father[node] != node:
-            path.append(node)
-            node = father[node]
+        while father[nodeIndex] != nodeIndex:
+            path.append(nodeIndex)
+            nodeIndex = father[nodeIndex]
         for n in path:
-            father[n] = node
-        return node
+            father[n] = nodeIndex
+        return nodeIndex
+
+    def point_to_int(self, x: int, y: int, m: int) -> int:
+        return x * m + y

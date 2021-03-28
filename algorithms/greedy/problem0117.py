c47f3e98e64761970d498ae2a54c097399f37404
Super262 <virusformail@gmail.com>
2021-03-28 19:38:12 +0800

    Refined Problem 117.

--- algorithms/greedy/problem0117.py
+++ algorithms/greedy/problem0117.py
@@ -9,16 +9,16 @@ class Solution:
         first_start = 0
         while first_start < len(a) - 1:
             next_start_choices = range(first_start + 1, first_start + a[first_start] + 1)
-            next_start = 0
-            max_jump = 0
+            next_start = first_start + 1
+            max_jump = next_start + a[next_start]
             for ch in next_start_choices:
                 if len(a) - 1 == ch:
                     return result + 1
-                jump = ch + a[ch]
-                if jump <= max_jump:
+                temp_jump = ch + a[ch]
+                if temp_jump <= max_jump:
                     continue
                 next_start = ch
-                max_jump = jump
+                max_jump = temp_jump
             result += 1
             first_start = next_start
         return result

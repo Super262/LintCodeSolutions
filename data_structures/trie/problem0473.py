e14f6f4e0c6b186364ee65316a37d41b2e9288db
Super262 <virusformail@gmail.com>
2021-03-29 10:22:33 +0800

    Refined Problem 473.

---
+++ data_structures/trie/problem0473.py
@@ -0,0 +1,47 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.sons = {}
+        self.is_word = False
+        self.word = ""
+
+    """
+    @param: word: Adds a word into the data structure.
+    @return: nothing
+    """
+
+    def addWord(self, word: str) -> None:
+        p = self
+        for ch in word:
+            if ch not in p.sons:
+                p.sons[ch] = WordDictionary()
+            p = p.sons[ch]
+        p.is_word = True
+        p.word = word
+
+    """
+    @param: word: A word could contain the dot character '.' to represent any one letter.
+    @return: if the word is in the data structure.
+    """
+
+    def search(self, word: str) -> bool:
+        return self.find(word, 0)
+
+    def find(self, word: str, start_index: int) -> bool:
+        if start_index == len(word) - 1:
+            if word[start_index] == ".":
+                for ch in self.sons:
+                    if self.sons[ch].is_word:
+                        return True
+            else:
+                return word[start_index] in self.sons and self.sons[word[start_index]].is_word
+        else:
+            if word[start_index] == ".":
+                for ch in self.sons:
+                    if self.sons[ch].find(word, start_index + 1):
+                        return True
+            else:
+                if word[start_index] not in self.sons:
+                    return False
+                return self.sons[word[start_index]].find(word, start_index + 1)
+        return False

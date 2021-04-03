a83290cd511fcd80f228a7d260c77a15042163cb
Super262 <virusformail@gmail.com>
2021-04-03 14:21:44 +0800

    Finished Problem 85.

---
+++ data_structures/tree/problem0085.py
@@ -0,0 +1,32 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: node: insert this node into the binary search tree
+    @return: The root of the new binary search tree.
+    """
+
+    def insertNode(self, root: TreeNode, node: TreeNode) -> TreeNode:
+        if not root:
+            root = node
+            return root
+        cur_node = root
+        while cur_node:
+            if cur_node.val == node.val:
+                break
+            if cur_node.val > node.val:
+                if not cur_node.left:
+                    cur_node.left = node
+                    break
+                cur_node = cur_node.left
+            else:
+                if not cur_node.right:
+                    cur_node.right = node
+                    break
+                cur_node = cur_node.right
+        return root

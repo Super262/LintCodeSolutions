43918acd2db249e94860af55d117a600b2eb95d3
Super262 <virusformail@gmail.com>
2021-04-04 22:22:51 +0800

    Finished Problem 130.

--- data_structures/heap/problem0130.py
+++ data_structures/heap/problem0130.py
@@ -1,17 +1,19 @@
 class Solution:
-    """
-    @param: A: Given an integer array
-    @return: nothing
-    """
-
-    def siftup(self, A, k):
-        while k > 0:
-            father = (k - 1) // 2
-            if A[k] > A[father]:
+    # @param A: Given an integer array
+    # @return: void
+    def siftdown(self, A, k):
+        while k * 2 + 1 < len(A):
+            son = k * 2 + 1  # A[i]左儿子的下标
+            if k * 2 + 2 < len(A) and A[son] > A[k * 2 + 2]:
+                son = k * 2 + 2  # 选择两个儿子中较小的一个
+            if A[son] >= A[k]:
                 break
-            A[k], A[father] = A[father], A[k]
-            k = father
+
+            temp = A[son]
+            A[son] = A[k]
+            A[k] = temp
+            k = son
 
     def heapify(self, A):
-        for i in range(len(A)):
-            self.siftup(A, i)
+        for i in range((len(A) - 1) // 2, -1, -1):
+            self.siftdown(A, i)

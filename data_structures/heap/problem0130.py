f6f7fe9754934507e4d951f6b419f55eb0eed9d2
Super262 <virusformail@gmail.com>
2021-04-04 22:15:47 +0800

    Finished Problem 130.

--- data_structures/heap/problem0130.py
+++ data_structures/heap/problem0130.py
@@ -4,28 +4,14 @@ class Solution:
     @return: nothing
     """
 
-    def heapify(self, a: list) -> None:
-        # 自底向上，从最后1个父节点到根节点
-        if not a or len(a) < 2:
-            return
-        root_indices = []
-        for i in range(len(a)):
-            if 2 * i + 1 < len(a):
-                root_indices.append(i)
-        root_indices.reverse()
-        for current_root in root_indices:
-            while True:
-                left_child = 2 * current_root + 1
-                if left_child >= len(a):
-                    break
-                right_child = left_child
-                if right_child < len(a) - 1:
-                    right_child += 1
-                if a[current_root] <= a[left_child] and a[current_root] <= a[right_child]:
-                    break
-                if a[left_child] <= a[right_child]:
-                    a[current_root], a[left_child] = a[left_child], a[current_root]
-                    current_root = left_child
-                else:
-                    a[current_root], a[right_child] = a[right_child], a[current_root]
-                    current_root = right_child
+    def siftup(self, A, k):
+        while k != 0:
+            father = (k - 1) // 2
+            if A[k] > A[father]:
+                break
+            A[k], A[father] = A[father], A[k]
+            k = father
+
+    def heapify(self, A):
+        for i in range(len(A)):
+            self.siftup(A, i)

47dfec9f2bfe12ec307eb1e83d2f02ddbb01beb2
Super262 <virusformail@gmail.com>
2021-04-10 20:16:30 +0800

    Finished Problem 724.

--- algorithms/dp/problem0724.py
+++ algorithms/dp/problem0724.py
@@ -9,11 +9,9 @@ class Solution:
         m = sum_of_nums // 2
         n = len(nums)
         dp = [0] * (m + 1)
-        min_dis = sum_of_nums
         for i in range(1, n + 1):
             for j in range(m, -1, -1):
                 if j < nums[i - 1]:
                     break
                 dp[j] = max(dp[j - nums[i - 1]] + nums[i - 1], dp[j])
-                min_dis = min(min_dis, sum_of_nums - 2 * dp[j])
-        return min_dis
+        return sum_of_nums - 2 * dp[m]

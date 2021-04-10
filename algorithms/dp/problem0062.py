e5656ed1fce1d930912aace3bae96622be01486b
Super262 <virusformail@gmail.com>
2021-04-10 10:03:11 +0800

    Finished Problem 62.

---
+++ algorithms/dp/problem0062.py
@@ -0,0 +1,18 @@
+class Solution:
+    """
+    @param m: positive integer (1 <= m <= 100)
+    @param n: positive integer (1 <= n <= 100)
+    @return: An integer
+    """
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+        for i in range(n):
+            dp[0][i] = 1
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m - 1][n - 1]

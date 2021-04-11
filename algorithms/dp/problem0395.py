ded80a5931dc6593e6ab70018110eb371903c5bd
Super262 <virusformail@gmail.com>
2021-04-11 12:44:58 +0800

    Finished Problem 395.

--- algorithms/dp/problem0395.py
+++ algorithms/dp/problem0395.py
@@ -5,4 +5,11 @@ class Solution:
     """
 
     def firstWillWin(self, values: list) -> bool:
-
+        n = len(values)
+        f2 = 0
+        f1 = values[n - 1] - f2
+        for i in range(n - 2, -1, -1):
+            f0 = max(values[i] - f1, values[i] + values[i + 1] - f2)
+            f2 = f1
+            f1 = f0
+        return f1 >= 0

e52ce1a88c97ebb719d91d43a2bfcb8fcf5f997d
Super262 <virusformail@gmail.com>
2021-04-11 08:42:06 +0800

    Finished Problem 512.

--- algorithms/dp/problem0512.py
+++ algorithms/dp/problem0512.py
@@ -5,16 +5,20 @@ class Solution:
     """
 
     def numDecodings(self, s: str) -> int:
+        if not s:
+            return 0
         n = len(s)
-        if n == 0:
-            return 1
-        f = [0] * (n + 1)
-        f[0] = 1
+        f0 = 1
+        f1 = 0
         if s[0] != "0":
-            f[1] = 1
+            f1 = 1
+        f2 = 0
         for i in range(2, n + 1):
             if s[i - 1] != "0":
-                f[i] += f[i - 1]
+                f2 += f1
             if s[i - 2] != "0" and 10 <= int(s[i - 2: i]) <= 26:
-                f[i] += f[i - 2]
-        return f[n]
+                f2 += f0
+            f0 = f1
+            f1 = f2
+            f2 = 0
+        return f1

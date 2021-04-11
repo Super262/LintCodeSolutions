9856db1e8110561aaa18f2455583124f354aefb2
Super262 <virusformail@gmail.com>
2021-04-12 00:06:58 +0800

    Finished Problem 77.

--- algorithms/dp/problem0077.py
+++ algorithms/dp/problem0077.py
@@ -12,9 +12,11 @@ class Solution:
         l2 = len(b)
         dp = [[0] * (l2 + 1) for _ in range(2)]
         for i in range(1, l1 + 1):
+            new_i = i % 2
+            old_i = (i - 1) % 2
             for j in range(1, l2 + 1):
                 if a[i - 1] == b[j - 1]:
-                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1
+                    dp[new_i][j] = dp[old_i][j - 1] + 1
                 else:
-                    dp[i % 2][j] = max(dp[(i - 1) % 2][j], dp[i % 2][j - 1])
+                    dp[new_i][j] = max(dp[old_i][j], dp[new_i][j - 1])
         return dp[l1 % 2][l2]

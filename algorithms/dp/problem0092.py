29714700e10ec00fcffc2c7e61f98d0c372665fa
Super262 <virusformail@gmail.com>
2021-04-12 20:00:54 +0800

    Reformatted code.

--- algorithms/dp/problem0092.py
+++ algorithms/dp/problem0092.py
@@ -5,18 +5,22 @@ class Solution:
     @return: The maximum size
     """
 
+    # 滚动数组优化
     def backPack(self, m: int, a: list) -> int:
         n = len(a)
-        dp = [[False] * (m + 1) for _ in range(n + 1)]
+        dp = [[False] * (m + 1) for _ in range(2)]
         dp[0][0] = True
         for i in range(1, n + 1):
-            dp[i][0] = True
+            new_i = i % 2
+            old_i = (i - 1) % 2
+            dp[new_i][0] = True
             for j in range(1, m + 1):
                 if j >= a[i - 1]:
-                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - a[i - 1]]
+                    dp[new_i][j] = dp[old_i][j] or dp[old_i][j - a[i - 1]]
                 else:
-                    dp[i][j] = dp[i - 1][j]
+                    dp[new_i][j] = dp[old_i][j]
+        n_reduced = n % 2
         for w in range(m, -1, -1):
-            if dp[n][w]:
+            if dp[n_reduced][w]:
                 return w
         return -1
--- algorithms/dp/problem0125.py
+++ algorithms/dp/problem0125.py
@@ -6,13 +6,16 @@ class Solution:
     @return: The maximum value
     """
 
+    # 滚动数组优化
     def backPackII(self, m: int, a: list, v: list) -> int:
         n = len(a)
-        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        dp = [[0] * (m + 1) for _ in range(2)]
         for i in range(1, n + 1):
+            new_i = i % 2
+            old_i = (i - 1) % 2
             for j in range(1, m + 1):
                 if j >= a[i - 1]:
-                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - a[i - 1]] + v[i - 1])
+                    dp[new_i][j] = max(dp[old_i][j], dp[old_i][j - a[i - 1]] + v[i - 1])
                 else:
-                    dp[i][j] = dp[i - 1][j]
-        return dp[n][m]
+                    dp[new_i][j] = dp[old_i][j]
+        return dp[n % 2][m]

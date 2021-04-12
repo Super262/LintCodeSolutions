6a0fb5016d857a2e286df834de34289b40093305
Super262 <virusformail@gmail.com>
2021-04-12 20:09:38 +0800

    Reformatted code.

--- algorithms/dp/problem0440.py
+++ algorithms/dp/problem0440.py
@@ -8,14 +8,16 @@ class Solution:
 
     def backPackIII(self, a: list, v: list, m: int) -> int:
         n = len(a)
-        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        dp = [[0] * (m + 1) for _ in range(2)]
         for i in range(1, n + 1):
+            new_i = i % 2
+            old_i = (i - 1) % 2
             for j in range(1, m + 1):
                 if j >= a[i - 1]:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - a[i - 1]] + v[i - 1])
+                    dp[new_i][j] = max(dp[old_i][j], dp[new_i][j - a[i - 1]] + v[i - 1])
                 else:
-                    dp[i][j] = dp[i - 1][j]
-        return dp[n][m]
+                    dp[new_i][j] = dp[old_i][j]
+        return dp[n % 2][m]
 
 # 优化后的多重背包
 # class Solution:

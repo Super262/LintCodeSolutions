57ce564771656f2ea2a5358b48f48cadc6632af0
Super262 <virusformail@gmail.com>
2021-04-13 20:16:56 +0800

    Finished Problem 508.

---
+++ algorithms/sorting/problem0508.py
@@ -0,0 +1,10 @@
+class Solution:
+    """
+    @param: nums: A list of integers
+    @return: nothing
+    """
+
+    def wiggleSort(self, nums) -> None:
+        for i in range(1, len(nums)):
+            if (i % 2 == 0 and nums[i] > nums[i - 1]) or (i % 2 == 1 and nums[i] < nums[i - 1]):
+                nums[i], nums[i - 1] = nums[i - 1], nums[i]

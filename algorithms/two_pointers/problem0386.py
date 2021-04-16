4dd32e9a9bb31e268f5b9b48d7091079f2078502
Super262 <virusformail@gmail.com>
2021-04-16 09:09:32 +0800

    Finished Problem 386.

--- algorithms/two_pointers/problem0386.py
+++ algorithms/two_pointers/problem0386.py
@@ -4,20 +4,19 @@ class Solution:
         if not s or k == 0:
             return 0
         ch_freq = [0] * 256
-        unique_count = 0
-        result = 0
+        current_unique_count = 0
+        answer = 0
         left = 0
         right = 0
         while right < len(s):
             ch_freq[ord(s[right])] += 1
             if ch_freq[ord(s[right])] == 1:
-                unique_count += 1
+                current_unique_count += 1
             right += 1
-            while left < right and unique_count > k:
+            while left < right and current_unique_count > k:
                 ch_freq[ord(s[left])] -= 1
                 if ch_freq[ord(s[left])] == 0:
-                    unique_count -= 1
+                    current_unique_count -= 1
                 left += 1
-            if right - left > result:
-                result = right - left
-        return result
+            answer = max(right - left, answer)
+        return answer

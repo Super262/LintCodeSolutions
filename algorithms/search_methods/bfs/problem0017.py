311dea4b0aa4e4f50e27fba80175057a73443269
Super262 <virusformail@gmail.com>
2021-04-18 14:57:04 +0800

    Finished Problem 17.

---
+++ algorithms/search_methods/bfs/problem0017.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param nums: A set of numbers
+    @return: A list of lists
+    """
+
+    def subsets(self, nums: list) -> list:
+        queue = [[]]
+        index = 0
+        while index < len(queue):
+            base_set = queue[index]
+            for num in nums:
+                if base_set and base_set[-1] >= num:
+                    continue
+                tmp = list(base_set)
+                tmp.append(num)
+                queue.append(tmp)
+            index += 1
+        return queue

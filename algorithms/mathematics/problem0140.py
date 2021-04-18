e9275015a25b11f438ed178337d5a8fad362659d
Super262 <virusformail@gmail.com>
2021-04-18 08:27:27 +0800

    Finished Problem 140.

---
+++ algorithms/mathematics/problem0140.py
@@ -0,0 +1,16 @@
+class Solution:
+    """
+    @param a: A 32bit integer
+    @param b: A 32bit integer
+    @param n: A 32bit integer
+    @return: An integer
+    """
+
+    def fastPower(self, a: int, b: int, n: int) -> int:
+        if n == 0:
+            return 1 % b
+        product = self.fastPower(a, b, n // 2)
+        product = (product * product) % b
+        if n % 2 == 1:
+            product = product * a % b
+        return product

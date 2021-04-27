def13cbdf1188dd7e578471928042c40d82a3221
Super262 <virusformail@gmail.com>
2021-04-27 20:07:06 +0800

    Reformatted code.

--- data_structures/hash_table/problem0056.py
+++ data_structures/hash_table/problem0056.py
@@ -7,7 +7,7 @@ class Solution:
 
     def twoSum(self, numbers: list, target: int) -> list:
         num_to_index = dict()
-        result_pair = set()
+        candidates = set()
         for i in range(len(numbers)):
             if numbers[i] in num_to_index:
                 # For repeating elements, only remember the first one.
@@ -15,7 +15,7 @@ class Solution:
             num_to_index[numbers[i]] = i
         for j in range(len(numbers)):
             t = target - numbers[j]
-            if t in result_pair:
+            if t in candidates:
                 return [num_to_index[t], j]
-            result_pair.add(numbers[j])
+            candidates.add(numbers[j])
         return [-1, -1]

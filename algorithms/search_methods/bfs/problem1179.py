8ce0de02f53304bd59a3ae97ef69e828ec3b809d
Super262 <virusformail@gmail.com>
2021-04-28 23:29:43 +0800

    Reformatted code.

--- algorithms/search_methods/bfs/problem1179.py
+++ algorithms/search_methods/bfs/problem1179.py
@@ -1,22 +1,27 @@
 class Solution:
+    """
+    @param M: a matrix
+    @return: the total number of friend circles among all the students
+    """
 
-    def findCircleNum(self, M: list) -> int:
-        if not M:
+    def findCircleNum(self, m: list) -> int:
+        if not m:
             return 0
+        visited = [False] * len(m)
         count = 0
-        for i in range(len(M[0])):
-            if M[i][i] == 1:
-                self.bfs(M, i)
+        for i in range(len(m)):
+            if m[i][i] == 1 and not visited[i]:
+                self.bfs(m, visited, i)
                 count += 1
         return count
 
-    def bfs(self, M: list, start: int) -> None:
+    def bfs(self, m: list, visited: list, start: int) -> None:
         import collections
         q = collections.deque([start])
         while q:
             node = q.popleft()
-            M[node][node] = 0
-            for i in range(len(M[node])):
-                if M[node][i] == 0 or M[i][i] == 0:
+            for i in range(len(m[node])):
+                if m[i][i] == 0 or m[node][i] == 0 or visited[i]:
                     continue
                 q.append(i)
+                visited[i] = True

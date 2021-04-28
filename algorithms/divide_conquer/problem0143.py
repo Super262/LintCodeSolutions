935869058e8700a7122cd69daf8a15a1ba078e74
Super262 <virusformail@gmail.com>
2021-04-28 12:32:20 +0800

    Reformatted code.

--- algorithms/divide_conquer/problem0143.py
+++ algorithms/divide_conquer/problem0143.py
@@ -5,20 +5,20 @@ class Solution:
             return
         self.sort_by_divide(colors, 0, len(colors) - 1, 1, k)
 
-    def sort_by_divide(self, colors: list, index_start: int, index_end: int, color_first: int, color_last: int) -> None:
-        if index_end == index_start or color_first == color_last:
+    def sort_by_divide(self, colors: list, start_index: int, end_index: int, first_color: int, last_color: int) -> None:
+        if end_index == start_index or first_color == last_color:
             return
-        color_mid = color_first + (color_last - color_first) // 2
-        i = index_start
-        j = index_end
+        mid_color = first_color + (last_color - first_color) // 2
+        i = start_index
+        j = end_index
         while i <= j:
-            while i <= j and colors[i] <= color_mid:  # 这里使用小于等于号，因为计算color_mid时结果偏左
+            while i <= j and colors[i] <= mid_color:  # 这里使用小于等于号，因为计算color_mid时结果偏左
                 i += 1
-            while i <= j and colors[j] > color_mid:
+            while i <= j and colors[j] > mid_color:
                 j -= 1
             if i <= j:
                 colors[i], colors[j] = colors[j], colors[i]
                 i += 1
                 j -= 1
-        self.sort_by_divide(colors, index_start, j, color_first, color_mid)
-        self.sort_by_divide(colors, i, index_end, color_mid + 1, color_last)
+        self.sort_by_divide(colors, start_index, j, first_color, mid_color)
+        self.sort_by_divide(colors, i, end_index, mid_color + 1, last_color)

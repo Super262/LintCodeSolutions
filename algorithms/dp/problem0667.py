ac3f79c24e62fb4f0f407e9946d210ce878dab0a
Super262 <virusformail@gmail.com>
2021-05-09 09:34:32 +0800

    Reformatted code.

--- algorithms/dp/problem0667.py
+++ algorithms/dp/problem0667.py
@@ -13,7 +13,7 @@ class Solution:
             seqLen[i][i] = 1
         # Note that we must iterate the string from its tail to its head to get
         # all elements for further steps.
-        for start in reversed(range(0, sLen)):
+        for start in range(sLen - 1, -1, -1):
             for end in range(start + 1, sLen):
                 if s[start] == s[end]:
                     seqLen[start][end] = seqLen[start + 1][end - 1] + 2

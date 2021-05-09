0692705999cf9ef5edb8e94809a52b930539f25d
Super262 <virusformail@gmail.com>
2021-05-09 10:35:05 +0800

    Reformatted code.

--- algorithms/two_pointers/problem0609.py
+++ algorithms/two_pointers/problem0609.py
@@ -7,7 +7,7 @@ class Solution:
 
     def twoSum5(self, nums: list, target: int) -> int:
         result = 0
-        nums.sort()
+        nums.sort()  # 千万不要忘记排序！
         start = 0
         end = len(nums) - 1
         while start < end:
@@ -15,5 +15,5 @@ class Solution:
                 end -= 1
             else:
                 result += end - start  # 这个区间内的所有数字均满足要求
-                start += 1
+                start += 1  # 千万不要忘记递增start！
         return result
